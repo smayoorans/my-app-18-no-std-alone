bc56d9576e63279180782c9f7038869e9bad4a23
Mayooran <Mayooran.Somasundaram@optimosoftware.co.uk>
2024-10-14 22:04:18 +0530

    Angular Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = "";
+
+  registerForm;
+
+  constructor(public fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+        firstName: ['', [Validators.required]],
+        lastName: [''],
+        email: ['', [Validators.required, Validators.email]],
+        phone: ['', [Validators.pattern("^\\d{10}$")]],
+    })
+  }
+
+  get form() {
+    return this.registerForm.controls;
+  }
+
+
+  onSubmit() {
+    let user = this.registerForm.value;
+    console.log(user);
+  }
+
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +12,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
